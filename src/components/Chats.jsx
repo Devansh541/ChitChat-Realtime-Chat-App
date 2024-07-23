092792b1da1994aac07f6d6f5fa43377325cbca7
Devansh <devanshkumar83@gmail.com>
2024-07-24 04:16:01 +0530

    Update Chat and Messages

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -13,6 +13,26 @@ const Chats = () => {
           <p>hello</p>
         </div>
       </div>
+      <div className="userChat">
+        <img
+          src="https://pbs.twimg.com/profile_images/1722717068502429697/5yH3heHq_400x400.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://pbs.twimg.com/profile_images/1722717068502429697/5yH3heHq_400x400.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   );
 };
